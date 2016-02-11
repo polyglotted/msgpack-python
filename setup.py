b824a02f8ed9324b42906ae67ef903bec0825a25
David Haines <david@polyglotted.co>
2016-02-11 20:18:45 +0000

    bumped

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='msgpack',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    version='1.0.0',
+    version='1.1.0',
     description='Python implementation of the MessagePack serialization format',
     author='David Haines',
     author_email='david@polyglotted.co',
     url='https://github.com/polyglotted/msgpack-python',
-    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.0.0',
+    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.1.0',
     keywords=['msgpack'],
     classifiers=[],
     install_requires=[
