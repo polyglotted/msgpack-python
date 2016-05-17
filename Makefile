690f263d3af3b76597e88d430fa9aefdf2426dbe
David Haines <david@polyglotted.co>
2016-05-18 00:04:49 +0100

    added pytest config and refactored test reqs

--- Makefile
+++ Makefile
@@ -1,10 +1,7 @@
 .DEFAULT_GOAL := test
 
-testdeps:
-	pip install -r test-requirements.txt
-
 install:
 	python setup.py install
 
 test:
-	py.test tests --html=test-report.html
+	python setup.py test
--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
 
-with open('test-requirements.txt') as f:
-    test_requirements = f.read().splitlines()
-
 setup(
     name='msgpack',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    version='1.3.0',
+    version='1.3.1',
     description='Python implementation of the MessagePack serialization format',
     author='David Haines',
-    author_email='david@polyglotted.io',
+    author_email='david@polyglotted.co',
     url='https://github.com/polyglotted/msgpack-python',
-    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.3.0',
+    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.3.1',
     keywords=['msgpack'],
     classifiers=[],
-    tests_require=test_requirements
+    setup_requires=[
+        'pytest-runner>=2.0,<3dev'
+    ],
+    tests_require=[
+        'coverage==4.1b2',
+        'pytest==2.9.1',
+        'pytest-html==1.8.0'
+    ]
 )
