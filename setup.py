31286fa9bfc88a01cc94cd6a2daf072ab311a45d
David Haines <david@polyglotted.co>
2016-05-18 14:37:10 +0100

    bumped

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('test-requirements.txt') as f:
 setup(
     name='msgpack',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    version='1.3.3',
+    version='1.3.4',
     description='Python implementation of the MessagePack serialization format',
     author='David Haines',
     author_email='david@polyglotted.co',
     url='https://github.com/polyglotted/msgpack-python',
-    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.3.3',
+    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.3.4',
     keywords=['msgpack'],
     classifiers=[],
     tests_require=requirements
