42e8df58ce90e8dae7cdb802586b8b7f22002adc
David Haines <david@polyglotted.co>
2016-05-18 13:00:51 +0100

    externalised test requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('test-requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='msgpack',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    version='1.3.2',
+    version='1.3.3',
     description='Python implementation of the MessagePack serialization format',
     author='David Haines',
     author_email='david@polyglotted.co',
     url='https://github.com/polyglotted/msgpack-python',
-    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.3.2',
+    download_url='https://github.com/polyglotted/msgpack-python/tarball/1.3.3',
     keywords=['msgpack'],
     classifiers=[],
-    setup_requires=[
-        'pytest-runner==2.7.1'
-    ],
-    tests_require=[
-        'coverage==4.1b2',
-        'pytest==2.9.1',
-        'pytest-html==1.8.0'
-    ]
+    tests_require=requirements
 )
